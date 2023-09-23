e75dbb0dd3bed0043b39c6f7e0d4c7995fe8f4e6
= <=>
2023-09-23 11:42:18 +0700

    EKF

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ try:
             if z is not None and extended_kfx is not None: 
                 comparation_ekf_data_["Measured"] = np.hstack((comparation_ekf_data_["Measured"], z.reshape(-1, 1)))
                 comparation_ekf_data_["Predicted"] = np.hstack((comparation_ekf_data_["Predicted"], extended_kfx.reshape(-1, 1)))
-            
+
             print("Waiting for commands...")
             time.sleep(0.5)
             firebase_listener()
